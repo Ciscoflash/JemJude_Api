18dd93a5fa20133ba5c84db4b14f2dbff390e023
Ciscoflash <abrahamjude1999@gmail.com>
2023-11-10 16:11:56 +0100

    implemented Google Captcha

--- server/index.ts
+++ server/index.ts
@@ -11,6 +11,7 @@ import { CreateTranscription } from "./services/Translation";
 import connect from "./config/Database";
 import path from "path";
 import cors from "cors";
+import axios from "axios";
 connect();
 const allowedOrigins = [
   "http://localhost:5173",
@@ -30,6 +31,32 @@ app.use("/api/v1", router);
 app.use("/api/v1", Localization);
 app.use("/api/v1", contactRouter);
 app.use("/api/v1", careerRouter);
+
+app.post("/validateRecaptcha", async (req, res) => {
+  try {
+    const { recaptchaToken, recaptchaText } = req.body;
+    // console.log(recaptchaToken, recaptchaText);
+    if (!recaptchaToken) {
+      return res.status(400).json("Recaptcha token is required");
+    }
+    const secretKey = "6Lf2LAopAAAAALtiExm7mUDd-mZjVI4cfs47bNTp";
+    const { data, status, statusText } = await axios.post(
+      `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${recaptchaToken}`
+    );
+    if (data.success) {
+      // continue some model save operations
+      console.log("Demo Model saved");
+      return res.json({ data, status, statusText });
+    } else {
+      console.log("Demo Model saved failed");
+      return res.status(400).json({ data, message: "Invalid recapthca" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.json({ error: error });
+  }
+});
+
 app.get("*", (req, res) => {
   return res.send("Request not found");
 });
